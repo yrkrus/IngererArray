920400744b670ec99a1946fbce6edb888423c574
yrkrus <yrk.rus@gmail.com>
2023-08-24 12:54:34 +0300

    #24.08.23

--- IntegerArray.cpp
+++ IntegerArray.cpp
@@ -7,6 +7,20 @@ IntegerArray::IntegerArray(int lenght) : lenght_(lenght)
 	
 }
 
+// инкремент
+IntegerArray &IntegerArray::operator++()
+{
+	lenght_++;
+	return *this;
+}
+IntegerArray &IntegerArray::operator--()
+{
+	lenght_--;
+	return *this;
+}
+
+
+
 ostream &operator<<(ostream &output, const IntegerArray &lenght)
 {
 
