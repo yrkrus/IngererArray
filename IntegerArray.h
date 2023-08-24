d9795bb8bfb82e11e0a117b6d444691404a14fc8
yrkrus <yrk.rus@gmail.com>
2023-08-24 12:52:19 +0300

    #24.08.23

--- IntegerArray.h
+++ IntegerArray.h
@@ -10,6 +10,12 @@ class IntegerArray
 public:
 	IntegerArray() = default;
 	IntegerArray(const int lenght);	
+	
+	// перегрузки инкрементов
+	IntegerArray &operator++();			
+	IntegerArray &operator--();			
+	IntegerArray operator++(int);		
+	IntegerArray operator--(int);		
 
 	~IntegerArray() {};
 
